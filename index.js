7affc227a7d1698dda7dc59284e4e433e5046622
Jazz Lyles <existenzial@Sam-o.local>
2018-08-11 19:29:42 -0700

    add bunyan logger for tests

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const puppeteer = require('puppeteer');
+const { log } = require('./utils');
 /*
 HEADLESS
 const browser = await puppeteer.launch();
 
 HEADFUL
-const browser = await puppeteer.launch({ headless: false });
+const browser = await puppeteer.launch({
+  headless: false,
+  devtools: true
+});
 */
 
-// Launch Headless Chrome
 puppeteer.launch()
   .then(async (browser) => {
-    // Open New Blank Page
+    /*
+    SETUP:
+      - Open a Blank Page
+      - Nav to URL
+    */
     const page = await browser.newPage();
-    // Navigate to Page Actual
     await page.goto('https://www.google.com');
-    // Take a Screenshot of Page
+
+    /*
+    PAGE TESTING:
+      - Viewport Dimensions
+    */
+    const dimensions = await page.evaluate(() => {
+      return {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        deviceScaleFactor: window.devicePixelRatio
+      };
+    });
+
+    log.info({ dimensions });
+
+    /*
+    VQA:
+      - Screenshots
+      - PDFs
+    */
     await page.screenshot({ path: './screenshots/google_test.png' });
-    // Close Browser
+    await page.pdf({ path: './pdfs/google_test.pdf', format: 'A4' });
+
+    // Exit
     await browser.close();
   });
---
+++ utils.js
@@ -0,0 +1,8 @@
+const log = require('bunyan').createLogger({
+  name: 'Puppeteer',
+  level: 2
+});
+
+module.exports = {
+  log,
+};
